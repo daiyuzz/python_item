947f28edffcaea217b4038212b938b8923b7d81a
daiyu <daiyu_zz@163.com>
2020-11-28 16:21:36 +0800

    11.28

--- FluentPython  / 第三部分：把函数视作对象/第5章：一等函数.py
+++ FluentPython  / 第三部分：把函数视作对象/第5章：一等函数.py
@@ -125,6 +125,7 @@ class BingoCage:
     def __call__(self, *args, **kwargs):
         return self.pick()
 
+
 bingo = BingoCage(range(3))
 
 print(bingo.pick())
@@ -137,3 +138,226 @@ print(callable(bingo))
 # True
 
 
+"""
+实现__call__方法的类是创建函数类对象的简便方式，此时必须在内部维护一个状态，让它在
+调用之间可用
+"""
+
+"================================================================================"
+# 5.6 函数内省
+print(dir(factorial))
+
+## dict
+
+"""
+与用户定义的常规类一样，函数适用 __dict__ 属性存储赋予它的用户属性。这相当于一种基于形式的注解。
+一般来说，为函数随意赋予属性不是很常见的做法
+
+
+在Django框架中，把 short_description 属性赋予一个方法，Django管理后台使用这个方法时，在记录
+列表中会出现指定的描述文本：
+"""
+
+
+def upper_case_name(obj):
+    return ("%s %s" % (obj.first_name, obj.last_name)).upper()
+
+
+upper_case_name.short_description = 'Customer name'
+
+"""
+下面重点说明函数专有而用户定义的一般对象没有的属性
+"""
+
+
+class C: pass
+
+
+obj = C()
+
+
+def func(): pass
+
+
+print(sorted(set(dir(func)) - set(dir(obj))))
+# 常规对象没有而函数有的属性
+# ['__annotations__', '__call__', '__closure__', '__code__', '__defaults__', '__get__', '__globals__', '__kwdefaults__', '__name__', '__qualname__']
+
+
+"=================================================================================="
+
+# 5.7 从定位参数到仅限关键字参数
+
+"""
+Python 最好的特性之一时提供了极为灵活的参数处理机制，而且Python3 进一步提供了仅限关键字参数。与之
+密切相关的是，调用函数使用 * 和 ** 展开可迭代对象，映射到单个参数
+
+"""
+
+
+def tag(name, *content, cls=None, **attrs):
+    """生成一个或多个HTML标签"""
+    if cls is not None:
+        attrs['class'] = cls
+    if attrs:
+        attr_str = ''.join('%s="%s"' % (attr, value) for attr, value in sorted(attrs.items()))
+    else:
+        attr_str = ''
+    if content:
+        return '\n'.join('<%s%s>%s</%s>' % (name, attr_str, c, name) for c in content)
+    else:
+        return '<%s%s />' % (name, attr_str)
+
+
+print(tag('br'))  # 传入单个定位参数，生成一个指定名称的空标签
+# <br/>
+
+print(tag('p', 'hello'))  # 第一个参数后面的任意个参数会被 *content 捕获，存入一个元祖
+# <p>hello</p>
+
+print(tag('p', 'hello', 'world'))
+# <p>hello</p>
+# <p>world</p>
+
+print(tag('p', 'hello', id=33))  # tag函数签名中没有明确指定名称的关键字参数会被 **attrs 捕获，存入一个字典
+# <pid="33">hello</p>
+
+print(tag('p', 'hello', 'world', cls='sidebar'))  # cls参数只能作为关键字参数传入
+# <pclass="sidebar">hello</p>
+# <pclass="sidebar">world</p>
+
+print(tag(content='testing', name='img'))  # 调用tag函数时，即便第一个定位参数也能作为关键字参数传入
+# <img content="testing" />
+
+
+my_tag = {'name': 'img', 'title': 'Jack', 'src': 'sunset.jpg', 'cls': 'framed'}
+print(tag(**my_tag))  # 在my_tag 前面加上 ** ，字段中的所有元素作为单个参数传入，同名键会绑定到对应的具名参数上，余下的则被 **attrs 捕获
+# <img class="framed"src="sunset.jpg"title="Jack" />
+
+
+"""
+仅限关键字参数是 python3 新增的特性，上面cls参数只能通过关键字参数指定，它一定不会捕获未命名的定位参数。
+
+定义函数时若想指定仅限关键字参数，要把它们放到前面有 * 的参数后面。如果不想支持数量不定的定位参数，
+但想支持仅限关键字参数，在签名中放一个*，如下所示
+"""
+
+
+def f(a, *, b):
+    return a, b
+
+
+print(f(1, b=2))
+# (1,2)
+
+"""
+注意：仅限关键字参数不一定要有默认值，可以像上面例子中b那样，强制必须传入实参。
+"""
+
+"======================================================================================="
+# 5.8 获取关于参数的信息
+
+
+import bobo
+
+
+@bobo.query('/')
+def hello(person):
+    return 'Hello %s!' % person
+
+
+"""
+bobo.query 装饰器把一个普通的函数（如hello）与框架的请求处理机制结合起来。BoBo会内省hello函数，
+发现它需要一个名为person的参数，然后从请求中获取那个名称对应的参数，将其传给hello参数，因此程序员根本
+不用触碰请求对象
+
+
+Bobo是怎么知道函数需要哪个参数呢？它又是怎么知道参数有没有默认值呢？
+
+
+函数对象有一个 __defaults__ 属性，它的值是一个元祖，里面保存着定位参数和关键字参数的默认值。
+仅限关键字参数的默认值在 __kwdefaults__ 属性中。然而，参数的名称在 __code__ 属性中，它的值是一个code
+对象引用，自身也有很多属性。
+"""
+
+
+def clip(text, max_len=80):
+    """在max_len前面或后面的第一个空格处截断文本"""
+    end = None
+    if len(text) > max_len:
+        space_before = text.rfind(' ', 0, max_len)
+        if space_before >= 0:
+            end = space_before
+        else:
+            space_after = text.rfind(' ', max_len)
+            if space_after >= 0:
+                end = space_after
+    if end is None:
+        end = len(text)
+    return text[:end].rstrip()
+
+
+"======================================================================================="
+
+
+# 5.9 函数注解
+
+def clip(text: str, max_len: 'int>0' = 80) -> str:
+    """在max_len前面或后面的第一个空格处截断文本"""
+    end = None
+    if len(text) > max_len:
+        space_before = text.rfind(' ', 0, max_len)
+        if space_before >= 0:
+            end = space_before
+        else:
+            space_after = text.rfind(' ', max_len)
+            if space_after >= 0:
+                end = space_after
+    if end is None:
+        end = len(text)
+    return text[:end].rstrip()
+
+
+"""
+有注解的函数声明
+
+函数声明中的各参数可以在：之后增加注解表达式。如果参数有默认值，直接放在参数名和=号之间。
+如果想注解返回值，在 ） 和函数声明末尾的 ： 之间添加 -> 和一个表达式。那个表达式可以使任何类型的
+
+注解不会做任何处理，只是存储在函数的 __annotations__ 属性（一个字典）中
+
+
+
+"""
+
+print(clip.__annotations__)
+# {'text': <class 'str'>, 'max_len': 'int>0', 'return': <class 'str'>}
+
+
+"=================================================================================="
+# 支持函数式编程的包
+
+## operator模块
+
+
+"""
+在函数式编程中，经常需要把算术运算符当做函数使用
+"""
+
+from functools import reduce
+
+
+def fact(n):
+    return reduce(lambda a, b: a * b, range(1, n + 1))
+
+
+"""
+operator模块为多个算数运算符提供了对应的函数，从而避免编写lambda a,b:a*b 这种平凡的匿名函数
+"""
+
+from functools import reduce
+from operator import mul
+
+
+def fact(n):
+    return reduce(mul, range(1, n + 1))
---
+++ FluentPython  / 第三部分：把函数视作对象/第6章：使用一等函数实现设计模式.py
@@ -0,0 +1,192 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2020/11/28 下午2:23
+# @Author  : daiyu
+# @File    : 第6章：使用一等函数实现设计模式.py
+# @Software: PyCharm
+
+
+"""
+《设计模式：可复用面向对象软件的基础》一书是这样概述“策略”模式
+的：
+定义一系列算法，把它们一一封装起来，并且使它们可以相互替
+换。本模式使得算法可以独立于使用它的客户而变化
+
+
+
+电商领域有个功能明显可以使用“策略”模式，即根据客户的属性或订单
+中的商品计算折扣
+
+
+
+假如一个网店制定了下述折扣规则。
+有 1000 或以上积分的顾客，每个订单享 5% 折扣。
+同一订单中，单个商品的数量达到 20 个或以上，享 10% 折扣。
+订单中的不同商品达到 10 个或以上，享 7% 折扣。
+简单起见，我们假定一个订单一次只能享用一个折扣。
+"""
+
+# 实现Order类，支持插入式折扣策略
+
+from abc import ABC, abstractmethod
+from collections import namedtuple
+
+Customer = namedtuple('Customer', 'name fidelity')
+
+
+class LineItem:
+    def __init__(self, product, quantity, price):
+        self.product = product
+        self.quantity = quantity
+        self.price = price
+
+    def total(self):
+        return self.price * self.quantity
+
+
+class Order:
+    def __init__(self, customer, cart, promotion=None):
+        self.customer = customer
+        self.card = list(cart)
+        self.promotion = promotion
+
+    def total(self):
+        if not hasattr(self, '__total'):
+            self.__total = sum(item.total() for item in self.card)
+        return self.__total
+
+    def due(self):
+        if self.promotion is None:
+            discount = 0
+        else:
+            discount = self.promotion.discount(self)
+        return self.total() - discount
+
+    def __repr__(self):
+        fmt = '<Order total:{:.2f} due:{:.2f}>'
+        return fmt.format(self.total(), self.due())
+
+
+class Promotion(ABC):  # 策略：抽象基类
+
+    @abstractmethod
+    def discount(self, order):
+        """返回折扣金额"""
+
+
+class FidelityPromo(Promotion):  # 第一个具体策略
+    """积分为1000或以上的顾客提供5%折扣"""
+
+    def discount(self, order):
+        return order.total() * .05 if order.customer.fidelity >= 1000 else 0
+
+
+class BulkItemPromo(Promotion):  # 第二个具体策略
+    """单个商品为20个或以上时提供10%折扣"""
+
+    def discount(self, order):
+        discount = 0
+        for item in order.card:
+            if item.quantity >= 20:
+                discount += item.total() * .1
+        return discount
+
+
+class LargeOrderPromo(Promotion):  # 第三个具体策略
+    """订单中的不同商品达到10个或以上时提供7%折扣"""
+
+    def discount(self, order):
+        distinct_items = {item.product for item in order.card}
+        if len(distinct_items) >= 10:
+            return order.total() * .07
+        return 0
+
+
+"""
+我们把 Promotion 定义为抽象基类（Abstract BaseClass， ABC），这么做是为了使用 @abstractmethod装饰器
+从而表明所用的模式
+
+"""
+
+joe = Customer('John', 0)
+ann = Customer('Ann Smith', 1100)
+cart = [LineItem('bababa', 4, .5), LineItem('apple', 10, 1.5), LineItem('watermellon', 5, 5.0)]
+print(Order(joe, cart, FidelityPromo()))
+# <Order total:42.00 due:42.00>
+
+print(Order(ann, cart, FidelityPromo()))
+# <Order total:42.00 due:39.90>
+
+banana_cart = [LineItem('banana', 30, .5), LineItem('apple', 10, 1.5)]
+print(Order(joe, banana_cart, BulkItemPromo()))
+# <Order total:30.00 due:28.50>
+
+long_order = [LineItem(str(item_code), 1, 1.0) for item_code in range(10)]
+
+print(Order(joe, long_order, LargeOrderPromo()))
+# <Order total:10.00 due:9.30>
+
+print(Order(joe, cart, LargeOrderPromo()))
+# <Order total:42.00 due:42.00>
+
+
+## 6.1.2 使用函数实现“策略”模式
+
+from collections import namedtuple
+
+Customer = namedtuple('Customer', 'name fidelity')
+
+
+class LineItem:
+    def __init__(self, product, quantity, price):
+        self.product = product
+        self.quantity = quantity
+        self.price = price
+
+    def total(self):
+        return self.price * self.quantity
+
+
+class Order:  # 上下文
+    def __init__(self, customer, cart, promotion=None):
+        self.customer = customer
+        self.cart = list(cart)
+        self.promotion = promotion
+
+    def total(self):
+        if not hasattr(self, '__total'):
+            self.__total = sum(item.total() for item in self.cart)
+        return self.__total
+
+    def due(self):
+        if self.promotion is None:
+            discount = 0
+        else:
+            discount = self.promotion(self)
+        return self.total() - discount
+
+    def __repr__(self):
+        fmt = '<Order total: {:.2f} due: {:.2f}>'
+        return fmt.format(self.total(), self.due())
+
+
+def fidelity_promo(order):
+    """为积分1000或以上的顾客提供5%折扣"""
+    return order.total() * .05 if order.customer.fidelity >= 100 else 0
+
+
+def bulk_item_promo(order):
+    """单个商品为20个或以上时提供10%折扣"""
+    discount = 0
+    for item in order.card:
+        if item.quantity >= 20:
+            discount += item.total() * .1
+    return discount
+
+
+def large_order_promo(order):
+    """订单中的不同商品达到10个或以上时提供7%折扣"""
+    distinct_items = {item.product for item in order.card}
+    if len(distinct_items) >= 10:
+        return order.total() * .07
+    return 0
+
