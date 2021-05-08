97698859561c828ac4e8b6123e5c5519e57b8da8
daiyu <daiyu_zz@163.com>
2021-05-08 09:16:33 +0800

    feat:将装饰器定义为类

---
+++ python-cookeBook/元编程/9.将装饰器定义为类.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2021/5/8 上午8:40
+# @Author  : daiyu
+# @File    : 9.将装饰器定义为类.py
+# @Software: PyCharm
+
+import types
+from functools import wraps
+
+
+class Profiled:
+
+    def __init__(self, func):
+        wraps(func)(self)
+        self.ncalls = 0
+
+    def __call__(self, *args, **kwargs):
+        self.ncalls += 1
+        return self.__wrapped__(*args, **kwargs)
+
+    def __get__(self, instance, owner):
+        if instance is None:
+            return self
+        else:
+            return types.MethodType(self, instance)  # 将方法动态的绑定到实例上
+
+
+@Profiled
+def add(x, y):
+    return x + y
+
+
+class Spam:
+    @Profiled
+    def bar(self, x):
+        print(self, x)
+
+
+print(add(2, 3))
+# 5
+
+print(add(4, 5))
+# 9
+
+print(add.ncalls)
+# 2
+
+s = Spam()
+print(s.bar(1))
+# <__main__.Spam object at 0x7f3b9ffdbcd0> 1
+
+print(s.bar(2))
+# <__main__.Spam object at 0x7f3b9ffdbcd0> 2
+
+print(s.bar(3))
+
+# <__main__.Spam object at 0x7f3b9ffdbcd0> 3
+
+print(Spam.bar.ncalls)
+# 3
